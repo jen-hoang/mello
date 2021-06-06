55ef97563bab6e26ca2111c491c678a3e8162c81
Jen <jenhoang304@gmail.com>
2021-06-06 11:42:31 -0700

    Delete unnecssary code in TaskListColumn component

--- src/components/TaskListColumn.vue
+++ src/components/TaskListColumn.vue
@@ -2,9 +2,7 @@
   <draggable
     class="dragArea list-group"
     :list="taskList"
-    :clone="clone"
     :group="{ name: 'card', pull: true }"
-    @start="start"
   >
     <task-dialog v-for="task in taskList" :key="task.id" :task="task" />
   </draggable>
@@ -12,7 +10,7 @@
 <script>
 import draggable from "vuedraggable";
 import TaskDialog from "./TaskDialog.vue";
-let idGlobal = 8;
+//let idGlobal = 8;
 export default {
   name: "task-list-column",
   props: {
@@ -21,17 +19,6 @@ export default {
   components: {
     "task-dialog": TaskDialog,
     draggable: draggable,
-  },
-  methods: {
-    clone(value) {
-      return { id: idGlobal++, ...value };
-    },
-    pullFunction() {
-      return this.controlOnStart ? "clone" : true;
-    },
-    start({ originalEvent }) {
-      this.controlOnStart = originalEvent.ctrlKey;
-    },
   },
 };
 </script>
