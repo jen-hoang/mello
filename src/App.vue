ce9cc5e813035c7354529aa8b4a8886fc293811d
Jen <jenhoang304@gmail.com>
2021-06-06 08:49:11 -0700

    init app layout with app bar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,35 @@
 <template>
-  <div id="app">
+  <!-- <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
-  </div>
+  </div> -->
+
+
+  <!-- App.vue -->
+
+<v-app>
+  <v-app-bar app dense flat>
+    <v-icon>mdi-home</v-icon>
+  </v-app-bar>
+
+  <!-- Sizes your content based upon application components -->
+  <v-main>
+
+    <!-- Provides the application the proper gutter -->
+    <v-container fluid>
+
+      <!-- If using vue-router -->
+      <router-view></router-view>
+    </v-container>
+  </v-main>
+
+  <v-footer app>
+    <!-- -->
+  </v-footer>
+</v-app>
 </template>
 
 <style lang="scss">
