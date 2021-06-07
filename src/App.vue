69d8d05deef5f0a39f49b275cc5c3d89b7d33709
Jen <jenhoang304@gmail.com>
2021-06-07 09:19:21 -0700

    Adjust background color of the web app

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,30 @@
 <template>
-  <!-- <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div> -->
-
-  <!-- App.vue -->
-
   <v-app>
-    <v-app-bar app dense flat>
-      <v-toolbar-title>Mello</v-toolbar-title>
-      <v-spacer></v-spacer>
-     <v-btn href="/" icon> <v-icon>mdi-home</v-icon></v-btn>
-    </v-app-bar>
+    <div class="grey lighten-4 mello-app-container">
+      <v-app-bar app flat color="transparent">
+        <v-toolbar-title class="text-h4">Mello</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn href="/" icon> <v-icon>mdi-home</v-icon></v-btn>
+      </v-app-bar>
 
-    <!-- Sizes your content based upon application components -->
-    <v-main>
-      <!-- Provides the application the proper gutter -->
-      <v-container fluid>
-        <!-- If using vue-router -->
-        <router-view></router-view>
-      </v-container>
-    </v-main>
+      <!-- Sizes your content based upon application components -->
+      <v-main>
+        <!-- Provides the application the proper gutter -->
+        <v-container fluid>
+          <!-- If using vue-router -->
+          <router-view></router-view>
+        </v-container>
+      </v-main>
 
-    <v-footer app>
-      <!-- -->
-    </v-footer>
+      <v-footer app>
+        <!-- -->
+      </v-footer>
+    </div>
   </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+<style lang="scss" scoped>
+.mello-app-container {
+  min-height: 100vh;
 }
 </style>
